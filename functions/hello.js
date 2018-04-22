2812f48d53a1f5e14ae1a650d2b81b8d6ae231eb
Harold Perry <hp@crushresearch.com>
2018-04-22 10:20:43 -0500

    Added headers.

--- functions/hello.js
+++ functions/hello.js
@@ -70,9 +70,15 @@
 "use strict";
 
 
+const hdrs = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type"
+};
+
 exports.handler = function (event, context, callback) {
     callback(null, {
         statusCode: 200,
+        headers: hdrs,
         body: "Hello, World"
     });
 };
--- netlify-lambda/hello.js
+++ netlify-lambda/hello.js
@@ -1,6 +1,12 @@
+const hdrs = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type"
+};
+
 exports.handler = function(event, context, callback) {
     callback(null, {
         statusCode: 200,
+        headers: hdrs,
         body: "Hello, World"
     });
 }
