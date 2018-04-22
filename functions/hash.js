45474462879d6a4fc1354a26b014925ef90ccfa7
Harold Perry <hp@crushresearch.com>
2018-04-22 10:04:54 -0500

    Added back netlify-lambda (no crypt)

---
+++ functions/hash.js
@@ -0,0 +1,114 @@
+(function(e, a) { for(var i in a) e[i] = a[i]; }(exports, /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+// require('dotenv').config();
+// const crypto = require('crypto'); 
+
+const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type"
+};
+
+exports.handler = (event, context, callback) => {
+    let nonce = event.queryStringParameters.nonce;
+    let output;
+    if (nonce) {
+        output = getJsonOutput("200", "OK", hashWithNonce(nonce));
+    } else {
+        output = getJsonOutput("400", "Required querystring property is missing.", "");
+    }
+    if (process.env._DEBUG === "true") {
+        output.debug = getDebugInfo(nonce, event, context);
+    }
+    callback(null, {
+        "isBase64Encoded": false,
+        "statusCode": output.status,
+        "headers": headers,
+        "body": JSON.stringify(output)
+    });
+};
+
+function hashWithNonce(nonce) {
+    // return crypto.createHmac("SHA256", process.env.API_SECRET).update(nonce).digest("base64");    
+    return "Hash";
+}
+
+function getJsonOutput(status, message, hash) {
+    return { "status": status, "message": message, "hash": hash };
+}
+
+function getDebugInfo(nonce, event, context) {
+    return { "apiKey": process.env.API_KEY, "event": event, "context": context };
+}
+
+/***/ })
+/******/ ])));
