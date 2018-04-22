f69b6482cf406866aa5b3022d63ffb74b4e9413f
Harold Perry <hp@crushresearch.com>
2018-04-22 10:31:37 -0500

    Removed functions

--- netlify-lambda/hello.js
+++ netlify-lambda/hello.js
@@ -7,27 +7,27 @@ exports.handler = function(event, context, callback) {
     let nonce = event.queryStringParameters.nonce;
     let output;
     if (nonce) {
-        output = getJsonOutput("200", "OK", hashWithNonce(nonce));
+        // output = getJsonOutput("200", "OK", hashWithNonce(nonce));
     } else {
-        output = getJsonOutput("400", "Required querystring property is missing.", "");
+        // output = getJsonOutput("400", "Required querystring property is missing.", "");
     }
 
     callback(null, {
         statusCode: 200,
         headers: hdrs,
-        body: output
+        body: "Hello There"
     });
 }
 
-function hashWithNonce(nonce) {
-    // return crypto.createHmac("SHA256", process.env.API_SECRET).update(nonce).digest("base64");    
-    return "Hash";
-}
+// function hashWithNonce(nonce) {
+//     // return crypto.createHmac("SHA256", process.env.API_SECRET).update(nonce).digest("base64");    
+//     return "Hash";
+// }
 
-function getJsonOutput(status, message, hash) {
-    return { "status": status, "message": message, "hash": hash };
-}
+// function getJsonOutput(status, message, hash) {
+//     return { "status": status, "message": message, "hash": hash };
+// }
 
-function getDebugInfo(nonce, event, context) {
-    return { "apiKey": process.env.API_KEY, "event": event, "context": context };
-}
+// function getDebugInfo(nonce, event, context) {
+//     return { "apiKey": process.env.API_KEY, "event": event, "context": context };
+// }
