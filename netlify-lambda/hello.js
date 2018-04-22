569b18d6c30c02cd4052ec49411b50a0c81de313
Harold Perry <hp@crushresearch.com>
2018-04-22 10:25:56 -0500

    Added private functions (no crypt)

--- netlify-lambda/hello.js
+++ netlify-lambda/hello.js
@@ -4,9 +4,30 @@ const hdrs = {
 };
 
 exports.handler = function(event, context, callback) {
+    let nonce = event.queryStringParameters.nonce;
+    let output;
+    if (nonce) {
+        output = getJsonOutput("200", "OK", hashWithNonce(nonce));
+    } else {
+        output = getJsonOutput("400", "Required querystring property is missing.", "");
+    }
+
     callback(null, {
         statusCode: 200,
         headers: hdrs,
         body: ("Hello, World " + process.env.API_KEY)
     });
+}
+
+function hashWithNonce(nonce) {
+    // return crypto.createHmac("SHA256", process.env.API_SECRET).update(nonce).digest("base64");    
+    return "Hash";
+}
+
+function getJsonOutput(status, message, hash) {
+    return { "status": status, "message": message, "hash": hash };
+}
+
+function getDebugInfo(nonce, event, context) {
+    return { "apiKey": process.env.API_KEY, "event": event, "context": context };
 }
